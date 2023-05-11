72ec2109e096c6a43bcb84797dbfc93846d3fb2e
Koi-AS <lightningblade.901@gmail.com>
2023-05-10 23:28:42 -0400

    Adding routing for products and tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,23 +3,24 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   try {
-    const categoryData = Category.findAll({
+    const categoryData = await Category.findAll({
       // be sure to include its associated Products
       include: [{ model: Product }]
     });
+    
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
-    const categoryData = Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
 
@@ -34,20 +35,20 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = Category.create(req.body);
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(categoryData);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = Category.update({
+    const categoryData = await Category.update({
       where: {
         id: req.params.id
       }
@@ -64,10 +65,10 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = Category.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }

b9b0846aaf8c354759ddec11a1a6b55300aa4081
Koi-AS <lightningblade.901@gmail.com>
2023-05-10 22:32:05 -0400

    Added get and post requests for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,16 +5,43 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = Category.findAll({
+      // be sure to include its associated Products
+      include: [{ model: PrdouctTag }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = Category.findByPk(req.params.id, {
+      include: [{ model: ProductTag }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    };
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(categoryData);
+  }
 });
 
 router.put('/:id', (req, res) => {

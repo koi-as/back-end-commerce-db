50ab81bf308f69673ccaf00bfd63c2341fb71057
Koi-AS <lightningblade.901@gmail.com>
2023-05-11 10:12:07 -0400

    Fixed my get by id and update requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk({
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
 
@@ -50,7 +50,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update({
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id
       }
